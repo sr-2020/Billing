c604e3642e3e97d963384de7ff54594571d4f7d7
vivchenkov <vivchenkov@mzt.ru>
2020-03-11 20:43:34 +0300

    + log errors

--- BillingAPI/Controllers/EvarunApiController.cs
+++ BillingAPI/Controllers/EvarunApiController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -12,6 +13,37 @@ namespace BillingAPI.Controllers
 {
     public abstract class EvarunApiController : ControllerBase
     {
+        protected DataResult<T> RunAction<T>(Func<T> action)
+        {
+            var result = new DataResult<T>();
+            try
+            {
+                Console.WriteLine("Action started");
+                result.Data = action();
+                result.Status = true;
+                Console.WriteLine("Action finished");
+            }
+            catch (BillingException e)
+            {
+                Console.WriteLine("---------WARNING---------");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("---------WARNING---------");
+                result.Message = e.Message;
+                result.Status = false;
+                Response.StatusCode = 422;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("---------ERROR---------");
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("---------ERROR---------");
+                result.Message = ex.ToString();
+                result.Status = false;
+                Response.StatusCode = 500;
+            }
+            return result;
+        }
+
         protected Result RunAction(Action action)
         {
             var result = new Result();
@@ -28,36 +60,12 @@ namespace BillingAPI.Controllers
             }
             catch (Exception ex)
             {
-                result.Message = ex.ToString();
-                result.Status = false;
-                Response.StatusCode = 500;
-            }
-
-            return result;
-        }
-
-        protected DataResult<T> RunAction<T>(Func<T> action)
-        {
-            var result = new DataResult<T>();
-            try
-            {
-                result.Data = action();
-                result.Status = true;
-            }
-            catch (BillingException e)
-            {
-                result.Message = e.Message;
-                result.Status = false;
-                Response.StatusCode = 422;
-            }
-            catch (Exception ex)
-            {
+                Debug.Fail(ex.ToString());
                 result.Message = ex.ToString();
                 result.Status = false;
                 Response.StatusCode = 500;
             }
             return result;
         }
-
     }
 }
--- BillingAPI/Startup.cs
+++ BillingAPI/Startup.cs
@@ -50,8 +50,8 @@ namespace BillingAPI
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        
         {
+            Console.WriteLine("configuring");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
