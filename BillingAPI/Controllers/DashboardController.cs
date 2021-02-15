1e053eb45351e5a69d602cc8c2a67ac2593a3c06
vivchenkov <vivchenkov@mzt.ru>
2021-02-15 23:04:19 +0300

    + dashboardcontroller

---
+++ BillingAPI/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Billing;
+using Billing.DTO;
+using BillingAPI.Model;
+using IoC;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillingAPI.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class DashboardController : EvarunApiController
+    {
+        [Route("a-shops")]
+        //checkrights
+        public DataResult<List<ShopDto>> GetShops()
+        {
+            var manager = IocContainer.Get<IShopManager>();
+            var result = RunAction(() => manager.GetShops(s => true));
+            return result;
+        }
+    }
+}
