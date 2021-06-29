53377fbc608aa60bdb876bd717e229311ae33b93
vivchenkov <vivchenkov@mzt.ru>
2021-06-29 20:40:56 +0300

    factors

--- Billing/Dto/Scoring/ScoringFactorDto.cs
+++ Billing/Dto/Scoring/ScoringFactorDto.cs
@@ -11,9 +11,17 @@ namespace Billing.Dto.Scoring
         {
             Id = factor.Id;
             Name = factor.Name;
+            CategoryId = factor.Category?.Id;
+            CategoryName = factor.Category?.Name;
+            CategoryType = factor.Category?.CategoryType;
+            CategoryWeight = factor.Category?.Weight;
         }
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Value { get; set; }
+        public int? CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int? CategoryType { get; set; }
+        public decimal? CategoryWeight { get; set; }
     }
 }
