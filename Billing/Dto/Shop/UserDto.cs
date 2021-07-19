cdc0ba5194702d74bf17439afdee88a004fd2ea9
vivchenkov <vivchenkov@mzt.ru>
2021-07-19 21:36:54 +0300

    user model to int

--- Billing/Dto/Shop/UserDto.cs
+++ Billing/Dto/Shop/UserDto.cs
@@ -14,19 +14,19 @@ namespace Billing.Dto.Shop
             Name = sin.Passport?.PersonName;
             if (sin.Character != null)
             {
-                ModelId = sin.Character.Model.ToString();
+                ModelId = sin.Character.Model;
                 Id = sin.Character.Model;
             }
             else
             {
-                ModelId = sin.Passport?.Sin;
+                ModelId = 0;
             }
             Balance = sin.Wallet?.Balance ?? 0;
             Eversion = sin.EVersion;
         }
         public int Id { get; set; }
         public string Name { get; set; }
-        public string ModelId { get; set; }
+        public int ModelId { get; set; }
         public decimal Balance { get; set; }
         public string Rights { get; set; }
         public string Eversion { get; set; }
